08ff7cf7e0b0e8354b6a8bc553b508472cff0401
Kofi Adutwum <kadut001@odu.edu>
2021-08-31 18:02:18 -0400

    added api

--- src/api.js
+++ src/api.js
@@ -5,8 +5,8 @@ axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 
 
 const instance = axios.create({
-    // baseURL: 'http://192.168.172.146',
-    baseURL: 'http://127.0.0.1:5000/'
+    baseURL: 'http://192.168.172.200/',
+    // baseURL: 'http://127.0.0.1:5000/'
 });
 
 export default instance;
